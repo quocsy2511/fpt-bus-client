519bc2eb68ab9c6f1a4b07394f6efb02e87b5206
Nguyễn Quốc sỹ <quocsy2511@gmail.com>
2023-03-21 09:59:00 +0700

    lasted

--- src/components/form/BusRouteForm.js
+++ src/components/form/BusRouteForm.js
@@ -102,12 +102,7 @@ const RouteForm = ({
     };
 
     const handleStationsBetweenChange = (index, value) => {
-        console.log('value', value)
-        console.log('index', index)
-
         const newStationsBetween = [...stationsBetween];
-        console.log('value between ', value)
-
         if (value === startStation || value === endStation) {
             setErrorDuplication(true)
             message.error('The station in the middle cannot match the selected station! please choose again');
